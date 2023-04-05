d9e86c8ff7576790efa675279c1aec350696e4d0
NikPeg <peganov.nik@gmail.com>
2023-04-05 03:31:33 +0300

    Hello world chatGPT example

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello")
+import os
+import openai
+import config
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": "Say 'Hello world!'"}
+  ]
+)
+
+print(completion.choices[0].message.content)
